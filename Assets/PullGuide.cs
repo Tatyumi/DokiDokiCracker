f154c10776b6823af9a9c978004902762dd0c147
Tatyumi <s150204@nishitech.ac.jp>
2019-01-09 21:04:33 +0900

    コメント追加
    不足していたコメントを追加

--- Assets/PullGuide.cs
+++ Assets/PullGuide.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 
 public class PullGuide : MonoBehaviour
 {
-
+    /// <summary>次動作までの間隔</summary>
     private float nextTime;
     /// <summary>点滅秋期</summary>
     public float interval = 1.0f;
-
 
     // Use this for initialization
     void Start()
@@ -20,6 +19,7 @@ public class PullGuide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //次の動作時間に達した場合
         if (Time.time > nextTime)
         {
             this.GetComponent<SpriteRenderer>().enabled = !this.GetComponent<SpriteRenderer>().enabled;
