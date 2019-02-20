d7737e342e270700ae44beccd6f8f11102a02e6e
Tatyumi <s150204@nishitech.ac.jp>
2019-02-20 17:45:19 +0900

    新規追加
    StartPanel.csの名前を変更後のクラス

---
+++ Assets/StartPanelController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPanelController : MonoBehaviour {
+
+    /// <summary>StartTextの横サイズ</summary>
+    private const float START_TEXT_SIZE_X = 700;
+    /// <summary>StartTextの縦サイズ</summary>
+    private const float START_TEXT_SIZE_Y = 300;
+}
