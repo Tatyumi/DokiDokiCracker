c4b1374b36b6065a1327a1334479569b238de494
Tatyumi <s150204@nishitech.ac.jp>
2019-02-20 17:44:03 +0900

    新規追加
    EndEffect.csの名前を変更後のクラス

---
+++ Assets/EndEffectController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndEffectController : MonoBehaviour
+{
+
+    /// <summary>エンドエフェクトの回転速度</summary>
+    private const float ROLLING_SPEED = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // エンドエフェクトを回転
+        transform.Rotate(0, 0, ROLLING_SPEED);
+    }
+}
