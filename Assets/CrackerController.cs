21692deb73053ced3fa07d35ed91961fc3eaf125
Tatyumi <s150204@nishitech.ac.jp>
2019-02-20 17:36:54 +0900

    新規追加
    imgCracker.csの名前を変更後のクラス

---
+++ Assets/CrackerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrackerController : MonoBehaviour
+{
+
+    /// <summary>クラッカーの画像</summary>
+    public Image imageCracker;
+    /// <summary>クラッカーの初期FILLOUT値</summary>
+    private const float CRACKER_FILLAMOUNT = 0.97f;
+
+    // Use this for initialization
+    void Start()
+    {
+        imageCracker.fillAmount = CRACKER_FILLAMOUNT;
+
+    }
+}
