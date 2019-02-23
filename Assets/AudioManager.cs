e7a305898a922861387d0c600aacf265edde1c68
Tatyumi <s150204@nishitech.ac.jp>
2019-02-23 15:27:23 +0900

    シングルトンに変更
    シーンに1つのみ存在するようにシングルトンに変更

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -2,36 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
     AudioSource audioSource;
-    /// <summary>田井徹画面のBGM</summary>
-    public AudioClip TitleBgm;
+    /// <summary>タイトルシーンのBGM</summary>
+    public AudioClip TitleSceneBGM;
     /// <summary>クラッカーの発射音</summary>
     public AudioClip CrackerFire;
     /// <summary>プレイシーンのBGM</summary>
-    public AudioClip PlayBgm;
+    public AudioClip PlaySceneBGM;
+    /// <summary>全オウディオ保持ディクショナリ</summary>
+    private Dictionary<string, AudioClip> AudioDic;
+
+    private void Awake()
+    {
+        // 存在確認
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+
+        //シーンが遷移しても破棄されない
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        if (PlaySceneBGM)
+        {
+            Debug.Log("よし");
+        }
+
+        // オウディオを格納
+        AudioDic = new Dictionary<string, AudioClip> {
+            { TitleSceneBGM.name, TitleSceneBGM},
+            { CrackerFire.name, CrackerFire },
+            { PlaySceneBGM.name, PlaySceneBGM },
+        };
+    }
 
     // Use this for initialization
     void Start()
     {
-        //シーンが遷移しても破棄されない
-        DontDestroyOnLoad(gameObject);
     }
 
     /// <summary>
     /// タイトルシーンのBGMを再生
     /// </summary>
-    public void StartTitleBgm()
+    //public void StartTitleBgm()
+    //{
+    //    audioSource = GetComponent<AudioSource>();
+    //    audioSource.PlayOneShot(TitleBGM, 0.5f);
+    //}
+
+    /// <summary>
+    /// 音を流す
+    /// </summary>
+    /// <param name="soundName">音の名前</param>
+    public void PlaySound(string soundName)
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(TitleBgm, 0.5f);
+        // 名前のチェック
+        if (!AudioDic.ContainsKey(soundName))
+        {
+            Debug.Log(soundName + "という名前のSEがありません");
+            return;
+        }
+        audioSource.PlayOneShot(AudioDic[soundName]);
     }
 
     /// <summary>
     /// 音を停止
     /// </summary>
-    public void StopMusic()
+    public void StopSound()
     {
         audioSource.Stop();
     }
@@ -39,18 +82,18 @@ public class AudioManager : MonoBehaviour
     /// <summary>
     /// プレイシーンのBGMを再生
     /// </summary>
-    public void StartPlayBgm()
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(PlayBgm, 0.5f);
-    }
+    //public void StartPlayBgm()
+    //{
+    //    audioSource = GetComponent<AudioSource>();
+    //    audioSource.PlayOneShot(PlayBGM, 0.5f);
+    //}
 
     /// <summary>
     /// クラッカーの発射音を再生
     /// </summary>
-    public void StartCrackerFire()
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(CrackerFire, 0.5f);
-    }
+    //public void StartCrackerFire()
+    //{
+    //    audioSource = GetComponent<AudioSource>();
+    //    audioSource.PlayOneShot(CrackerFire, 0.5f);
+    //}
 }
